f15514ec6025d690b30bdf9d1acd9c0c43207f6f
Dipesh <dipesh.chaudhary657@gmail.com>
2024-05-27 13:48:28 +0530

    add 2 more approaches

--- youtube_questions/nETSETOS YT Channel/commonLettersInAString.py
+++ youtube_questions/nETSETOS YT Channel/commonLettersInAString.py
@@ -1,8 +1,9 @@
+# Approach-1
 def commonLetters(my_name,your_name):
-    lst = [i.lower() for i in my_name]
+    lst = [i for i in my_name.lower()]
     common_letters = []
-    for i in your_name:
-        if i.lower() in lst:
+    for i in your_name.lower():
+        if i in lst:
             common_letters.append(i)
     common_lst = []
     [common_lst.append(i) for i in common_letters if i not in common_lst]
@@ -11,3 +12,18 @@ def commonLetters(my_name,your_name):
 my_name = "Dipesh"
 your_name = "Deeksha"
 print(commonLetters(my_name,your_name))
+
+# Approach-2
+def commons(a,b):
+    c= []
+    for i in a.lower():
+        if i in b.lower():
+            c.append(i)
+    return c
+print(commons(my_name,your_name))
+
+# Approach-3
+def comms(a,b):
+    c = [i for i in a.lower() if i in b.lower()]
+    return c
+print(comms(my_name,your_name))
